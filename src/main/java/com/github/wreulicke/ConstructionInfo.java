11e29a88c11423cfaeaaade8909389982bf2a02a
Wreulicke <masaya2dies@gmail.com>
2017-02-17 06:23:20 +0900

    add spotless config

--- src/main/java/com/github/wreulicke/ConstructionInfo.java
+++ src/main/java/com/github/wreulicke/ConstructionInfo.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Wreulicke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.wreulicke;
 
 import java.beans.ConstructorProperties;
@@ -11,29 +34,28 @@ public class ConstructionInfo<T> {
   public final Map<String, Class<?>> namedType;
 
   public ConstructionInfo(Constructor<T> constructor) {
-    ConstructorProperties properties = Objects.requireNonNull(
-      constructor.getAnnotation(ConstructorProperties.class),
+    ConstructorProperties properties = Objects.requireNonNull(constructor.getAnnotation(ConstructorProperties.class),
       "construction target is not annotated ConstructorProperties");
-    
-    String[] names=properties.value();
-    
-    namedType=new HashMap<>();
-    
+
+    String[] names = properties.value();
+
+    namedType = new HashMap<>();
+
     Class<?>[] types = constructor.getParameterTypes();
     for (int i = 0; i < names.length; i++) {
       namedType.put(names[i], types[i]);
     }
-    
-    this.names=names;
+
+    this.names = names;
   }
 
   public Class<?> getType(String name) {
-    return Objects.requireNonNull(namedType.get(name), "cannot find name:"+name+"type");
+    return Objects.requireNonNull(namedType.get(name), "cannot find name:" + name + "type");
   }
-  
-  public <E> boolean isAssignable(String name, Object element){
+
+  public <E> boolean isAssignable(String name, Object element) {
     return getType(name).isInstance(element);
   }
-  
-  
+
+
 }
--- src/test/java/com/github/wreulicke/TinyCreatorTest.java
+++ src/test/java/com/github/wreulicke/TinyCreatorTest.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Wreulicke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.wreulicke;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,7 +45,7 @@ public class TinyCreatorTest {
     assertThat(object.getNumber()).isEqualTo(2);
     assertThat(object.getString()).isEqualTo("str");
   }
-  
+
   @Test
   public void testCreate2() {
     TinyCreator creator = new TinyCreator();
@@ -35,7 +58,7 @@ public class TinyCreatorTest {
     assertThat(object.number).isEqualTo(2);
     assertThat(object.string).isEqualTo("str");
   }
-  
+
   @Test
   public void testCreate3() {
     TinyCreator creator = new TinyCreator();
